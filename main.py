8c78141382959b4c44baff1fa38db4297c04f8ad
ShootGan <nawelpalepka@gmail.com>
2021-09-02 16:13:50 +0200

    add schemas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import databases, sqlalchemy
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
+import schemas
 import models
 from database import SessionLocal
 
@@ -19,11 +19,16 @@ def get_db():
 
 @app.get("/places")
 def get_all_places(db: Session = Depends(get_db)):
-    places = db.query(models.Object).all()
+    try:
+        places = db.query(models.Object).all()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=str(e))
+    if not places:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no places were found")
     return places
 
 
-@app.get("/places/{id}", status_code=200)
+@app.get("/places/{id}", response_model=schemas.PlaceSchema)
 def get_one_place(id: int, response: Response, db: Session = Depends(get_db)):
     place = db.query(models.Object).filter(models.Object.ObjectId == id).first()
     if not place:
