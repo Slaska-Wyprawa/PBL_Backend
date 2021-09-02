64d9881dfb36055dbf6707f62a673fee77cb1aef
Paszymaja <36695377+Paszymaja@users.noreply.github.com>
2021-09-02 16:10:49 +0200

    Database connection string in .env

--- database.py
+++ database.py
@@ -1,23 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from functools import lru_cache
 import os
+from config.config import Settings
+
 USER = os.getenv('DATABASE_USERNAME')
 PASSWORD = os.getenv('DATABASE_PASSWORD')
 ADDRESS = os.getenv('DATABASE_ADDRESS')
 PORT = os.getenv('DATABASE_PORT')
 NAME = os.getenv('DATABASE_NAME')
 
-#database_connenct_URL = F'mysql://{USER}:{PASSWORD}@157.158.40.129:3306/pblapk_live'
-database_connenct_URL = 'mysql://APK:Cpia9qOehPC]-Y!r@157.158.40.129:3306/pblapk_live?charset=utf8'
-#database_connenct_URL = os.getenv('CONNECT_STRING')
 
-print(database_connenct_URL)
+@lru_cache()
+def get_settings():
+    return Settings()
 
-engine = create_engine(database_connenct_URL)
+
+settings: Settings = get_settings()
+database_connection_url = settings.database_connection_url
+
+engine = create_engine(database_connection_url)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
 metadata = Base.metadata
-
