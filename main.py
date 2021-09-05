e60f10ba6925a2f452f7e5983716533e9cf81312
ShootGan <nawelpalepka@gmail.com>
2021-09-05 18:30:03 +0200

    przeniesone na sqlmodel, testy do zrobienia

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, status, Response, HTTPException, Query
 import schemas
 from database import engine
 from sqlmodel import Field, Session, SQLModel, create_engine, select
@@ -9,38 +8,47 @@ from typing import List, Optional
 app = FastAPI()
 
 
-
-
-@app.get("/places", response_model=List[models.Objects])
-def get_all_places():
+@app.get("/places", response_model=List[models.BaseObject])
+def get_all_places(offset: int = 0, limit: int = Query(default=30, lte=100)):
     try:
         with Session(engine) as session:
-            places = session.exec(select(models.Objects)).all()
+            places = session.exec(
+                select(models.Object.ObjectId, models.Object.Name, models.Object.Longitude, models.Object.Latitude,
+                       models.Object.Description, models.Object.ImagePath).offset(offset).limit(limit)).all()
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=str(e))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     if not places:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no places were found")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return places
 
-@app.get("/test")
-def get_test_model():
-    with Session(engine) as session:
-        test = session.exec(select(models.Disabledfacility)).all()
-    return  test
 
-'''
-@app.get("/places/{id}", response_model=schemas.PlaceSchema)
-def get_one_place(id: int, response: Response, db: Session = Depends(get_db)):
-    place = db.query(models.Object).filter(models.Object.ObjectId == id).first()
+@app.get("/places/{id}", response_model=models.Object)
+def get_one_place(id: int):
+    try:
+        with Session(engine) as session:
+            place = session.exec(select(models.Object).where(models.Object.ObjectId == id)).first()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     if not place:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'place with id {id} is not avilable')
     return place
-@app.get("/discount")
-def get_all_places(db: Session = Depends(get_db)):
+
+
+@app.get("/discount/{place_id}", response_model=models.DisabilitiesSchema)
+def get_one_place(place_id: int):
     try:
-        discout = db.query(models.Dstype).all()
+        with Session(engine) as session:
+            discounts = session.exec(
+                select(models.Object.ObjectId, models.Dstype.DSTDesc).select_from(models.Object).join(
+                    models.Discount).join(models.Dstype).where(models.Discount.ObjectId == place_id)).all()
+            discounts_list = {
+                'ObjectId': place_id,
+                'DiscountList': [discounts[x][1] for x in range(len(discounts))]
+            }
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=str(e))
-    if not discout:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no discout were found")
-    return discout'''
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    if not discounts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'no discounts for place with id {place_id}')
+    return discounts_list
+
+
--- models.py
+++ models.py
@@ -1,50 +1,74 @@
-from typing import Optional, List
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
+from typing import List, Optional
+
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
 
 
-class Dftypes(SQLModel, table=True):
-    DFTId: Optional[int] = Field(default=None, primary_key=True)
-    DFTDesc: str
+class DisabilitiesSchema(SQLModel):
+    ObjectId: int
+    DiscountList: List[str]
 
 
-class Dstypes(SQLModel, table=True):
-    DSTId: Optional[int] = Field(default=None, primary_key=True)
-    DSTDesc: str
-
-
-class Objects(SQLModel, table=True):
-    ObjectId: Optional[int] = Field(default=None, primary_key=True)
-    Name: str
-    Description: str
-    EasyAcces: bool
-    FreeEntry: bool
-    ImagePath: str
-    FreeParking: Optional[bool] = None
-    GMapLink: str
-    OPMapLink: Optional[str] = None
-    Longitude: float
-    Latitude: float
-
-
-class Disabledfacility(SQLModel, table=True):
+class Disabledfacilitie(SQLModel, table=True):
     __tablename__ = 'disabledfacilities'
 
     DFId: Optional[int] = Field(default=None, primary_key=True)
     DFObjectId: int = Field(default=None, foreign_key="objects.ObjectId")
-    DFType: int = Field(foreign_key='dftypes.DFTId')
+    DFType: int = Field(default=None, foreign_key="dftypes.DFTId")
+    object: "Object" = Relationship(back_populates="dftypes_link")
+    dftype: "Dftype" = Relationship(back_populates="objects_link")
 
-    object:List[Objects] = Relationship(Objects)A
-    dftype: List[Dftypes] = Relationship( Dftypes)
 
-
-'''
-class Discount(SQLModel):
+class Discount(SQLModel, table=True):
     __tablename__ = 'discounts'
 
-    DSId = Column(Integer, primary_key=True, unique=True)
-    ObjectId = Column(ForeignKey('objects.ObjectId', ondelete='CASCADE'), nullable=False, index=True)
-    DSType = Column(ForeignKey('dstypes.DSTId', ondelete='CASCADE'), nullable=False, index=True)
+    DSId: Optional[int] = Field(default=None, primary_key=True)
+    ObjectId: int = Field(default=None, foreign_key="objects.ObjectId")
+    DSType: int = Field(default=None, foreign_key="dstypes.DSTId")
 
-    dstype = relationship('Dstype')
-    object = relationship('Object')
-'''
+    object: "Object" = Relationship(back_populates="dstypes_link")
+    dstype: "Dstype" = Relationship(back_populates="objects_link")
+
+
+class Dftype(SQLModel, table=True):
+    __tablename__ = 'dftypes'
+
+    DFTId: int = Field(default=None, primary_key=True)
+    DFTDesc: str
+
+    objects_link: List[Disabledfacilitie] = Relationship(back_populates="dftype")
+    # objects: List["Objects"] = Relationship(back_populates='dftype', link_model=Disabledfacilities)
+
+
+class Dstype(SQLModel, table=True):
+    __tablename__ = 'dstypes'
+
+    DSTId: Optional[int] = Field(default=None, primary_key=True)
+    DSTDesc: str
+
+    objects_link: List[Discount] = Relationship(back_populates="dstype")
+
+
+# object: List["Objects"] = Relationship(back_populates='dstype', link_model=Discounts)
+
+class BaseObject(SQLModel):
+    ObjectId: Optional[int] = Field(default=None, primary_key=True)
+    Name: str
+    Description: str
+    ImagePath: str
+    Longitude: float
+    Latitude: float
+
+
+class Object(BaseObject, table=True):
+    __tablename__ = 'objects'
+
+    EasyAcces: bool
+    FreeEntry: bool
+    FreeParking: Optional[bool] = None
+    GMapLink: str
+    OPMapLink: Optional[str] = None
+
+    dftypes_link: List[Disabledfacilitie] = Relationship(back_populates="object")
+    dstypes_link: List[Discount] = Relationship(back_populates="object")
+    # dftype: List[Dftypes] = Relationship(back_populates='objects', link_model=Disabledfacilities)
+    # dstype: List[Dstypes] = Relationship(back_populates='object', link_model=Discounts)
